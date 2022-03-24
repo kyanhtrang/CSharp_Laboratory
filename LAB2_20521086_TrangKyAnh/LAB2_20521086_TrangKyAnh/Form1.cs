990e5ca3c88ba3ade078577f79837425a903b98c
Trang Kỳ Anh <20521086@gm.uit.edu.vn>
2022-03-24 07:43:28 +0700

    LAB2

---
+++ LAB2_20521086_TrangKyAnh/LAB2_20521086_TrangKyAnh/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB2_20521086_TrangKyAnh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
